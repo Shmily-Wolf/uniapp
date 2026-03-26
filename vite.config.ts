6553f894726be60402dbe37d54e776430d88c587
王冲 <3436971707@qq.com>
2026-03-26 23:19:53 +0800

    初始化

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+import uni from "@dcloudio/vite-plugin-uni";
+import { UnifiedViteWeappTailwindcssPlugin } from 'weapp-tailwindcss/vite'
+import UniPages from '@uni-helper/vite-plugin-uni-pages'
+import UniManifest from '@uni-helper/vite-plugin-uni-manifest'
+import tailwindcss from '@tailwindcss/postcss'
+import path from 'node:path'
+
+export default defineConfig({
+  plugins: [
+    UniPages(),
+    UniManifest(),
+    uni(),
+    UnifiedViteWeappTailwindcssPlugin({
+      rem2rpx: true,
+      cssEntries: [
+        // 你 @import "weapp-tailwindcss"; 那个文件绝对路径
+        path.resolve(import.meta.dirname, './src/app.css'),
+      ],
+    }),
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+      ],
+    },
+  },
+});
